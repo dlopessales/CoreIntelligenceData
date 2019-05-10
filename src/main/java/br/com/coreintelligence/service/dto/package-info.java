bede34bdf1ce6efb74984a73ad804ba73e29a41a
Danilo <35387644+dlopessales@users.noreply.github.com>
2019-05-10 20:26:00 -0300

    First

---
+++ src/main/java/br/com/coreintelligence/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package br.com.coreintelligence.service.dto;
---
+++ src/main/java/br/com/coreintelligence/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package br.com.coreintelligence.web.rest.vm;
